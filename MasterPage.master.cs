6d93fda86908a7c334ac2c9f3f4b6639e608e411
xindingqq <fengchen618@126.com>
2013-10-21 17:05:42 +0800

    upload
    sda

---
+++ MasterPage.master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class admindo_MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            ChangeLoginImage();
+        }           
+    }
+    /// <summary>
+    /// 鼠标经过改变Login图片按钮的图片
+    /// </summary>
+    private void ChangeLoginImage()
+    {
+
+        ImageButton1.Attributes.Add("onmouseover", "this.src='style/Images/b0001bl.png'");
+        ImageButton1.Attributes.Add("onmouseout", "this.src='style/Images/b0001b.png'");
+    }
+}
